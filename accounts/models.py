6ed4566020c6444aadaecf8dd1df0ce6f4f785d5
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2021-01-06 23:01:12 +0200

    Custom user in django

---
+++ accounts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    first_name=models.CharField(max_length=64)
+    last_name=models.CharField(max_length=64)
+    email=models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.username
